f8cc8ba802bbafcdac4da29d5aa912ea0e57d36f
TheoKevH <theodorekevinh@gmail.com>
2024-09-18 15:12:24 +0700

    login function

--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def show_main(request):
@@ -26,6 +30,32 @@ def create_mood_entry(request):
 
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
 
 def show_xml(request):
     data = MoodEntry.objects.all()
